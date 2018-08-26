ad53eddeb332b003bd447fd7e12c164b05d05a64
kamilgurgener <38161636+kamilgurgener@users.noreply.github.com>
2018-08-25 23:40:33 -0400

    Update SmokeTestRunner.java

--- src/test/java/com/prestashop/runners/SmokeTestRunner.java
+++ src/test/java/com/prestashop/runners/SmokeTestRunner.java
@@ -14,7 +14,7 @@ import cucumber.api.junit.Cucumber;
 		 
 		},
 		
-		tags="@smoke",
+		//tags="@smoke",
 		features="src/test/resources/com/prestashop/features", 
 		glue="com/prestashop/step_definitions"
 		
